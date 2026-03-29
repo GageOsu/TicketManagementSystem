182e089197dca515f9c6e5b6e2068d27fab75191
Daniil Morozov <daniilmorozovwork@gmail.com>
2026-03-30 01:55:20 +0300

    Создал Централизованное управление пакетами установил в проект API, openAPI установил в проект Infrastruct EF Core

---
+++ TicketManagementSystem.Infrastructure/Complaints/ComplaintConfiguration.cs
@@ -0,0 +1,5 @@
+using TicketManagementSystem.Domain.Complaints;
+using TicketManagementSystem.Domain.Statuses;
+using TicketManagementSystem.Domain.Users;
+
+namespace TicketManagementSystem.Infrastructure.Complaints;
